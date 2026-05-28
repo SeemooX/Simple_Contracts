0ee0d4eafffa9cdbd38d4ced36e77b3290014232
SeemooX <bjjsimox@gmail.com>
2026-05-28 12:43:28 +0100

    Building the positive contract that has a function checker of number

---
+++ contracts/positive.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Positive {
+    function estPositif(uint256 x) public pure returns (bool) {
+        return x > 0;
+    }
+}
+
+/* 0xE08871266e4a84bCe3753B3E8e42734813A8A4c1: contract address in sepolia-ether */
+/* 0x67085ab549337c9e853ebdf0a4ebf62440513ab103307d9e65a03b95c9f82224:  transcation hash in sepolia-ether */
