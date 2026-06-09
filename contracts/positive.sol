3cc61544a55dadf87db28c7a5656aa81b0ad744b
SeemooX <bjjsimox@gmail.com>
2026-06-09 21:53:54 +0100

    Redploying thee positive contract, since we put in the argument a uint256 type while we need int256 , in order to be able to evaluate negative numbers

--- contracts/positive.sol
+++ contracts/positive.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract Positive {
-    function estPositif(uint256 x) public pure returns (bool) {
+    function estPositif(int256 x) public pure returns (bool) {
         return x > 0;
     }
 }
 
-/* 0xE08871266e4a84bCe3753B3E8e42734813A8A4c1: contract address in sepolia-ether */
-/* 0x67085ab549337c9e853ebdf0a4ebf62440513ab103307d9e65a03b95c9f82224:  transcation hash in sepolia-ether */
+/* 0x631B3DAc82D88b284F63E8a741645bE3D9102622: contract address in sepolia-ether */
+/* 0xba1bca754aab6d9ad118f77fb7e851bccb8a106938455c407443d29cc8832d34:  transcation hash in sepolia-ether */
